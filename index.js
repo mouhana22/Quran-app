0222a61bc64d1e23b25d6cd5d40bf547d878fe5c
mouhana22 <mouhana1422@gmail.com>
2022-12-05 21:50:42 +0300

    third commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Excel = require("exceljs");
 
 var qArray = JSON.parse(fs.readFileSync("data.json").toString());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json()); // this is to accept data in json format
